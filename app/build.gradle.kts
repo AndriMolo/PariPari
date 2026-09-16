0aca33eace80ac0f99a95463c00ded5f9bd205c9
Andri Molo <a.molodduwaquiroz@campus.unimib.it>
2026-09-16 20:30:35 +0200

    fix Windows/MacOS incompatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,14 @@ android {
         viewBinding = true
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
